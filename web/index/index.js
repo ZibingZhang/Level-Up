922b8507b21f7550b3473dce27b4e49edb92d7a4
Anisa <anisaanuar98@gmail.com>
2020-02-08 16:31:48 -0500

    ACTUAL z-index change

--- web/index/index.js
+++ web/index/index.js
@@ -2,6 +2,10 @@ $('.player-bottom-hand .playing-card').click(function() {
 	$(this).toggleClass('selected');
 });
 
+$('.player-bottom-hand .playing-card-image').click(function() {
+	$(this).toggleClass('selected');
+});
+
 $('.clear-selection').click(function() {
 	$('.selected').removeClass('selected');
 });
